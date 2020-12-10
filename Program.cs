c976c8e52cbb2b834db95936a07914ff1b5b8684
David Lee <sea0920@hotmail.com>
2020-12-10 01:30:34 -0800

    Third commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Hangfire;
 using Hangfire.MemoryStorage;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace hangfire_test
 {
@@ -10,21 +12,88 @@ namespace hangfire_test
         {
             GlobalConfiguration.Configuration.UseMemoryStorage();
 
-            BackgroundJob.Enqueue(() => Console.WriteLine("Background Job!"));
-            RecurringJob.AddOrUpdate(() => Console.WriteLine("Recurring Job!"), Cron.Minutely);
-            var jobId = BackgroundJob.Schedule(() => Console.WriteLine("Delayed!"), TimeSpan.FromSeconds(5));
-            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine("Continuation!"));
+            Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Enqueueing tasks");
+            BackgroundJob.Enqueue(() => new Job().ShowMessage("Background Job!"));
+            RecurringJob.AddOrUpdate(() => new Job().ShowMessage($"Recurring Job!"), Cron.Minutely);
+            var jobId = BackgroundJob.Schedule(() => new Job().ShowMessage($"Delayed!"), TimeSpan.FromSeconds(5));
+            BackgroundJob.ContinueJobWith(jobId, () => new Job().ShowMessage($"Continuation!"));
 
             using (new BackgroundJobServer())
             {
-                Console.WriteLine("Hangfire Server started. Press ENTER to exit...");
+                Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Hangfire Server started. Press ENTER to exit...");
                 Console.ReadLine();
             }
         }
+
+        private void Test2()
+        {
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            var foregroundTask1 = Task.Run(() =>
+            {
+                Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Enqueueing tasks");
+                
+                // Look at the stack trace.
+                //  Notice that this Expression Tree gets serialized and saved into the DB
+                //  and then later gets executed using Reflection in one of the BackgroundJobServers.
+                BackgroundJob.Enqueue(() => new Job().ShowTrackTrace());
+            });
+
+            var foregroundTask2 = Task.Run(() =>
+            {
+                Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Enqueueing tasks");
+
+                // Look at the thread id and see how they are different from thread ids of foreground tasks.
+                for (int i = 0; i < 5; i++)
+                {
+                    BackgroundJob.Enqueue(() => new Job().ShowMessage("BackgroundJob!"));
+                }
+            });
+
+            var backgroundTask1 = Task.Run(() =>
+            {
+                using (new BackgroundJobServer())
+                {
+                    Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Hangfire Server started. Press ENTER to exit...");
+                    Console.ReadLine();
+                }
+            });
+
+            var backgroundTask2 = Task.Run(() =>
+            {
+                using (new BackgroundJobServer())
+                {
+                    Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): Hangfire Server started. Press ENTER to exit...");
+                    Console.ReadLine();
+                }
+            });
+
+            // Because there are two BackgroundJobServers picking up jobs, even if you press Enter once to terminate one BackgroundJobServer,
+            //  another one still picks up Jobs and run them.
+            Task.WaitAll(foregroundTask1, foregroundTask2, backgroundTask1, backgroundTask2);
+        }
+
+        public class Job
+        {
+            public Job()
+            {
+            }
+
+            public void ShowMessage(string message)
+            {
+                Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}): {message}!");
+            }
+
+            public void ShowTrackTrace()
+            {
+                Console.WriteLine($"{Environment.StackTrace}");
+            }
+        }
+
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Test1();
+            p.Test2();
         }
     }
 }
